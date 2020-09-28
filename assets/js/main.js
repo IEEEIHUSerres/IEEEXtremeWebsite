af5370e424356e26909c3c66ab40651d0e1b748a
Iordanis Kostelidis <kostelidis@ieee.org>
2020-09-28 21:31:42 +0300

    init for 2020

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@
         /* ==========================================================================
            countdown timer
            ========================================================================== */
-        jQuery('#clock').countdown('2019/10/19', function (event) {
+        jQuery('#clock').countdown('2020/10/24', function (event) {
             jQuery(this).html(event.strftime(''
                 + '<div class="time-entry days"><span>%-D</span> Days</div> '
                 + '<div class="time-entry hours"><span>%H</span> Hours</div> '
